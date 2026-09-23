f85c802ae0cc438a43107c932fd7af17ebeedc5b
gobat-charge <theobellamy.gobat@gmail.com>
2026-09-23 17:38:58 +0200

    Fix coordinates and opening hours for establishments
    Updated latitude and longitude for multiple establishments and adjusted Sunday opening hours.

--- src/data/establishments.js
+++ src/data/establishments.js
@@ -2,8 +2,8 @@ export const establishments = [
   {
     name: 'Café Hugo',
     address: '2 place de la Libération, 21000 Dijon',
-    latitude: 47.32107,
-    longitude: 5.04145,
+    latitude: 47.321407,
+    longitude: 5.040793,
     openingHours: [
       'Lun–Ven : 7h30–00h00',
       'Sam : 7h30–14h00',
@@ -14,31 +14,31 @@ export const establishments = [
   {
     name: 'SIMONE',
     address: '38 rue des Godrans, 21000 Dijon',
-    latitude: 47.32374,
-    longitude: 5.03862,
+    latitude: 47.324062,
+    longitude: 5.038639,
     openingHours: [
       'Lun–Sam : 10h00–00h00',
-      'Dim : à confirmer'
+      'Dim : fermé'
     ],
     logo: './image00005.jpeg',
   },
   {
     name: "DIJ'EAT",
     address: '6 impasse de Reggio, 21000 Dijon',
-    latitude: 47.34018,
-    longitude: 5.01984,
+    latitude: 47.338693,
+    longitude: 5.067522,
     openingHours: [
       'Lun–Jeu : 10h00–22h00',
       'Ven–Sam : 10h00–00h00',
-      'Dim : fermé sauf événement'
+      'Dim : fermé'
     ],
     logo: './image00006.jpeg',
   },
   {
     name: "M'Beer",
     address: '2A rue Jean-Baptiste Say, 21800 Chevigny-Saint-Sauveur',
-    latitude: 47.29920,
-    longitude: 5.11270,
+    latitude: 47.304543,
+    longitude: 5.114972,
     openingHours: [
       'Lun : fermé',
       'Mar–Jeu : 11h00–23h00',
@@ -50,8 +50,8 @@ export const establishments = [
   {
     name: 'Beer Country',
     address: "2 place du 30 Octobre et de la Légion d'Honneur, 21000 Dijon",
-    latitude: 47.32183,
-    longitude: 5.05292,
+    latitude: 47.321893,
+    longitude: 5.053099,
     openingHours: [
       'Lun–Ven : 10h00–02h00',
       'Sam–Dim : 15h00–02h00'
@@ -61,8 +61,8 @@ export const establishments = [
   {
     name: 'Byron Bay',
     address: '8 Petite rue de Pouilly, 21000 Dijon',
-    latitude: 47.32647,
-    longitude: 5.04794,
+    latitude: 47.327967,
+    longitude: 5.044617,
     openingHours: [
       'Mar–Mer : 18h00–01h30',
       'Jeu–Sam : 18h00–02h00',
